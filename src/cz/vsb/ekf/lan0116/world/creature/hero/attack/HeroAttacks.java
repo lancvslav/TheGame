31860f7eb059824441e67c5dc0dc1e9dcf651d1f
Vitezslav Lanc <lancvslav@gmail.com>
2016-11-08 21:44:59 +0100

    refactored code

--- src/cz/vsb/ekf/lan0116/world/creature/hero/attack/HeroAttacks.java
+++ src/cz/vsb/ekf/lan0116/world/creature/hero/attack/HeroAttacks.java
@@ -2,25 +2,9 @@ package cz.vsb.ekf.lan0116.world.creature.hero.attack;
 
 public interface HeroAttacks {
 
-//     float damageMultiplier;
-//    float staminaConsumption ;
-//
-//    public HeroAttacks(float damageMultiplier, float staminaConsumption) {
-//        this.damageMultiplier = damageMultiplier;
-//        this.staminaConsumption = staminaConsumption;
-//    }
-//
-//    public float getDamageMultiplier() {
-//        return damageMultiplier;
-//    }
-//
-//    public float getStaminaConsumption() {
-//        return staminaConsumption;
-//    }
+    float getDamageMultiplier();
 
- float getDamageMultiplier();
-
-float getStaminaConsumption();
+    float getStaminaConsumption();
 
 
 }
